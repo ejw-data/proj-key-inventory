0ffc73910f72fe037669d7408e5d7e47a33e969c
Erin Wills <ejw.data@gmail.com>
2023-11-04 10:52:06 -0500

    more advanced table structure before refactoring

--- sql/key.sql
+++ sql/key.sql
@@ -3,34 +3,142 @@ DROP TABLE IF EXISTS keys;
 DROP TABLE IF EXISTS access_pairs;
 DROP TABLE IF EXISTS access_codes;
 DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS room_amenities;
 DROP TABLE IF EXISTS room_classification CASCADE;
 DROP TABLE IF EXISTS key_owner;
-
--- Future work (to be added)
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS access_approvers;
+DROP TABLE IF EXISTS access_zones;
+DROP TABLES IF EXISTS buildings;
 
 
 
+-- USERS ------------------------------------------------------
+CREATE TABLE titles (
+	title_id SERIAL PRIMARY KEY,
+	title VARCHAR
+);
 
+INSERT INTO titles (title)
+VALUES ('faculty'),
+		('undergraduate student'),
+		('graduate student'),
+		('staff'),
+		('building manager'),
+		('department head'),
+		('dean'),
+		('senior administration'),
+		('facilities');
+
+CREATE TABLE roles (
+	role_id SERIAL PRIMARY KEY,
+	user_role VARCHAR
+);
+
+INSERT INTO roles (user_role)
+VALUES ('requester'),
+		('approver'),
+		('admin')
+
+CREATE TABLE users (
+	user_id SERIAL PRIMARY KEY,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	title VARCHAR REFERENCES titles (title_id),
+	user_role VARCHAR REFERENCES roles (user_role)
+);
+
+INSERT INTO users (first_name, last_name, title, user_role)
+VALUES ('erin', 'wills', 1, 1),
+		('will', 'wright', 1, 1),
+		('andrew', 'ng', 1, 1),
+		('bob', 'turtle', 5, 3),
+		('jake', 'powers', 9, 3);
+		
 
 CREATE TABLE key_owner (
-	owner_id SERIAL PRIMARY KEY,
-	owner_first_name VARCHAR,
-	owner_last_name VARCHAR
+	owner_id INT PRIMARY KEY REFERENCES users (user_id)
 );
+
+INSERT INTO key_owner (owner_id)
+VALUES (1),
+		(2),
+		(3);
 
 CREATE TABLE access_approvers (
 	access_approver_id SERIAL PRIMARY KEY,
-	approver_id INT REFERENCES key_owner (owner_id),
-	approved_by VARCHAR,
-	date_approved TIMESTAMP NOT NULL DEFAULT NOW()
+	approver_id INT REFERENCES users (user_id),
+	role_approved_by VARCHAR,
+	date_approved TIMESTAMP NOT NULL DEFAULT NOW(),
+	date_removed TIMESTAMP
 );
 
-INSERT INTO key_owner (owner_first_name, owner_last_name)
-VALUES ('erin', 'wills'),
-		('will', 'wright'),
-		('andrew', 'ng'),
-		('bob', 'turtle');
+INSERT INTO access_approvers (approver_id, role_approved_by)
+VALUES (4, 9)
+
+-- APPROVAL PROCESS ------------------------------------------------------------
+
+
+CREATE TABLE approval_status (
+	status_code SERIAL PRIMARY KEY,
+	status_code_name VARCHAR
+);
+
+INSERT INTO approval_status (status_code_name)
+VALUES ('REQUEST SUBMITTED'),
+		('REQUEST APPROVED'),
+		('KEY CREATED'),
+		('KEY READY FOR PICKUP'),
+		('KEY ISSUED');
+
+-- requests table:  request_id, user_id, space, approver, status, dates, reasoning
+-- logic updates record
+-- logic on update adds record to key_owner table the deletes record
+CREATE TABLE requests (
+	request_id SERIAL PRIMARY KEY,
+	user_id INT,
+	space_number_id INT,
+	access_approver_id INT,
+	status_code INT REFERENCES approval_status (status_code),
+	request_date TIMESTAMP NOT NULL DEFAULT NOW(),
+	approved_date TIMESTAMP
+);
+
+-- Form updates to show only appropriate approvers
+-- Submit Form
+-- Approver gets dashboard updates based on query using status code
+-- Approver approves and status changes
+-- Key shop gets dashboard updates based on query using status code
+-- Key shop makes key and enters key number and status updates and key/user_id updates
+-- User picks up key and status updates
+
+-- update requests status_code then update key_owner table then create key_junction entry 
+
+
+
+
+
+-- SPACE & GRANTED APPROVAL ------------------------------------------------------------
+
+CREATE TABLE buildings (
+	bulding_number INT PRIMARY KEY,
+	building_name VARCHAR,
+	building_description VARCHAR
+);
+
+INSERT INTO buildings (building_number, building_name, building_description)
+VALUES (24, 'Chemistry', 'Chemistry Department Research Space')
+
+
+CREATE TABLE approver_zones (
+	building_number PRIMARY KEY REFERENCES buildings (building_number),
+	access_approver_id PRIMARY KEY REFERENCES access_approvers (access_approver_id)
+);
+
+INSERT INTO approver_zones (building_number, access_approver_id)
+VALUES (24, 4)
 
 CREATE TABLE room_classification (
 	room_type_id INT PRIMARY KEY,
@@ -44,19 +152,37 @@ VALUES (201, 'private office'),
 		(220, 'shared office');
 
 CREATE TABLE rooms (
-	room_number INT PRIMARY KEY,
-	room_type INT REFERENCES room_classification (room_type_id),
-	room_projector BOOL,
-	room_seating INT
+	space_number_id INT PRIMARY KEY,
+	building_number INT REFERENCES buildings (building_number),
+	floor_number INT,
+	wing_number INT,
+	room_number INT,
+	room_type INT REFERENCES room_classification (room_type_id)
 );
 
-INSERT INTO rooms (room_number, room_type, room_projector, room_seating)
-VALUES (100, '210', TRUE, 50),
-		(201, '201', FALSE, 1),
-		(202, '220', FALSE, 4);
-		
+INSERT INTO rooms (space_number, building_number, floor_number, wing_number, room_number, room_type, room_projector, room_seating)
+VALUES ('24-01-01-01', 24, 01, 01, 01, 210),
+		('24-02-01-01', 24, 02, 01, 01, 201),
+		('24-02-01-02', 24, 02, 01, 02, 220);
+
+
+CREATE TABLE room_amenities (
+	space_number_id INT PRIMARY KEY REFERENCES rooms (space_number_id),
+	room_projector BOOL,
+	room_seating INT
+); 
+
+INSERT INTO room_amenities (space_number_id, room_projector, room_seating)
+VALUES ('24-01-01-01', TRUE, 50),
+		('24-02-01-01', FALSE, 1),
+		('24-02-01-02', FALSE, 4);
+
+
+
+-- ACCESS ASSIGNMENT -------------------------------------------------------------------
+
 CREATE TABLE access_codes (
-	access_code SERIAL PRIMARY KEY,
+	access_code_id SERIAL PRIMARY KEY,
 	access_description VARCHAR,
 	created_by VARCHAR,
 	authorized_by VARCHAR,
@@ -70,9 +196,9 @@ VALUES (1, 'Classroom', 'ejw', 'T. Bundy'),
 
 
 CREATE TABLE access_pairs (
-	access_code INT REFERENCES access_codes (access_code),
-	room_number INT REFERENCES rooms (room_number),
-	PRIMARY KEY (access_code, room_number) 
+	access_code INT REFERENCES access_codes (access_code_id),
+	space_number_id INT REFERENCES rooms (space_number_id),
+	PRIMARY KEY (access_code_id, space_number_id) 
 );
 
 SELECT * FROM access_codes;
