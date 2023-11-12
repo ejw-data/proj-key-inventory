7d0bf521bc1e20ebd948e0c559dc6c16994e0fe6
Erin Wills <ejw.data@gmail.com>
2023-11-12 16:36:29 -0600

    updated model to include all tables, need to check for consistency

--- models.py
+++ models.py
@@ -75,3 +75,186 @@ class Roles(db.Model):
 #     db.Column(db.String(25))
 #     db.Column(db.Date)
 #     db.Column(db.Float)
+
+
+class Approvers(db.Model):
+    """
+    Approvers
+    """
+
+    __bind_key__ = "key_inventory"
+    __tablename__ = "access_approvers"
+    access_approver_id = db.Column(db.Integer, primary_key=True)
+    approver_id = db.Column(db.Integer)
+    role_approved_by = db.Column(db.String(128))
+    date_approved = db.Column(db.Date)
+    date_removed = db.Column(db.Date)
+
+
+class Buildings(db.Model):
+    """
+    Building Info
+    """
+
+    __bind_key__ = "key_inventory"
+    __tablename__ = "buildings"
+    buidlding_number = db.Column(db.Integer, primary_key=True)
+    building_name = db.Column(db.String(128))
+    building_description = db.Column(db.String(128))
+
+
+class Rooms(db.Model):
+    """
+    Room Info
+    """
+
+    __bind_key__ = "key_inventory"
+    __tablename__ = "rooms"
+    space_number_id = db.Column(db.String(128), primary_key=True)
+    buidlding_number = db.Column(db.Integer)
+    floor_number = db.Column(db.Integer)
+    wing_number = db.Column(db.Integer)
+    room_number = db.Column(db.Integer)
+    room_type = db.Column(db.String(128))
+
+
+class Room_Classification(db.Model):
+    """
+    Room Classification
+    """
+
+    __bind_key__ = "key_inventory"
+    __tablename__ = "room_classification"
+    room_type_id = db.Column(db.Integer, primary_key=True)
+    room_type = db.Column(db.String(128))
+
+
+class Room_Amenities(db.Model):
+    """
+    Room Info
+    """
+
+    __bind_key__ = "key_inventory"
+    __tablename__ = "room_amentities"
+    space_number_id = db.Column(db.Integer, primary_key=True)
+    room_projector = db.Column(db.Boolean)
+    room_seating = db.Column(db.Integer)
+
+
+class Requests(db.Model):
+    """
+    Key Requests
+    """
+
+    __bind_key__ = "key_inventory"
+    __tablename__ = "requests"
+    request_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    space_number_id = db.Column(db.String(128))
+    building_number = db.Column(db.Integer)
+    access_approver_id = db.Column(db.Integer)
+    access_code_id = db.Column(db.Integer)
+    status_code = db.Column(db.Integer)
+    request_date = db.Column(db.Date)
+    approved_date = db.Column(db.Date)
+    approved = db.Column(db.Boolean)
+    approval_comment = db.Column(db.String(128))
+    rejection_comment = db.Column(db.String(128))
+
+
+class Keys_Created(db.Model):
+    """
+    Keys fabricated
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "keys_created"
+    key_number = db.Column(db.Integer, primary_key=True)
+    key_copy = db.Column(db.Integer)
+    access_code_id = db.Column(db.Integer)
+    fabrication_status_id = db.Column(db.Integer)
+
+
+class Key_Status(db.Model):
+    """
+    Key availability
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "key_status"
+    key_status_id = db.Column(db.Integer, primary_key=True)
+    key_status = db.Column(db.String(128))
+
+
+class Key_Orders(db.Model):
+    """
+    Approved keys being produced by key shop
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "key_status"
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    access_code_id = db.Column(db.Integer)
+
+
+class Key_Inventory(db.Model):
+    """
+    Key location
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "key_inventory"
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    key_number = db.Column(db.Integer)
+    key_copy = db.Column(db.Integer)
+    date_transferred = db.Column(db.Date)
+    date_returned = db.Column(db.Date)
+
+
+class Fabrication_Status(db.Model):
+    """
+    Status of Key being made
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "fabrication_status"
+    fabrication_status_id = db.Column(db.Integer, primary_key=True)
+    fabrication_status = db.Column(db.String(128))
+
+
+class Approver_Zones(db.Model):
+    """
+    Approvers responsible for spaces
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "approver_zones"
+    building_number = db.Column(db.Integer, primary_key=True)
+    access_approver_id = db.Column(db.Integer)
+
+
+class Approval_Status(db.Model):
+    """
+    Status of key request from request to handoff
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "approval_status"
+    status_code = db.Column(db.Integer, primary_key=True)
+    status_code_name = db.Column(db.String(128))
+
+
+class Access_Pairs(db.Model):
+    """
+    Access codes for specific doors
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "access_pairs"
+    access_code_id = db.Column(db.Integer, primary_key=True)
+    space_number_id = db.Column(db.Integer, primary_key=True)
+
+
+class Access_Codes(db.Model):
+    """
+    Specific codes
+    """
+    __bind_key__ = "key_inventory"
+    __tablename__ = "access_codes"
+    access_code_id = db.Column(db.Integer, primary_key=True)
+    access_description = db.Column(db.String(128))
+    created_by = db.Column(db.String(128))
+    authorized_by = db.Column(db.Date)
+    created_on = db.Column(db.Date)
